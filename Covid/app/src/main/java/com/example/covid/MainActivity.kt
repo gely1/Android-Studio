d90cf9bc8a17b7d35f57ce25000d6a6a8b2a7f00
Angélica <angelicasalaicesarriola@gmail.com>
2021-04-25 15:38:44 -0600

    Crear Tabs
    Crear Tabs - Clase 20/04/2021

--- Covid/app/src/main/java/com/example/covid/MainActivity.kt
+++ Covid/app/src/main/java/com/example/covid/MainActivity.kt
@@ -4,11 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    lateinit var txtUsuario: EditText
+
 
 
 
@@ -16,19 +17,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        txtUsuario=findViewById(R.id.usuario)
-        var txtPassword: EditText=findViewById(R.id.pass)
-        Log.e( "Prueba",txtUsuario.text.toString())
-        print(txtPassword.text)
-        login(txtUsuario.text.toString(), txtPassword.text.toString())
+        var btnLogin= findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener{
+            startActivity(Intent(this, Tabs::class.java))
+        }
     }
-    fun login(user:String, pass:String, isAdmin:Boolean=true, id:Int=-1){
-       if(user.equals("admin") && pass.equals("123")){
-            var intent: Intent=Intent(this, Principal::class.java)
-            startActivity(intent)
 
-       }else{
-           Toast.makeText(applicationContext,"Error Credenciales Inválidas", Toast.LENGTH_LONG).show()
-       }
-    }
 }
---
+++ Covid/app/src/main/java/com/example/covid/Tabs.kt
@@ -0,0 +1,27 @@
+package com.example.covid
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.example.covid.ui.main.SectionsPagerAdapter
+
+class Tabs : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tabs)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+
+
+    }
+}
